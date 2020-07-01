7ab1c7cb17c45c657c1ddf7b4fb786c0c9c5b210
Jeff Deards <jdeards84@gmail.com>
2020-06-30 19:08:05 -0700

    updated files and added new files

---
+++ index.js
@@ -0,0 +1,49 @@
+'use strict';
+
+// this function toggles the active class on the js-slider-menu
+function toggleSliderMenu() {
+    $('.js-slider-menu').toggleClass('active');
+}
+
+// this function checks to see if the slider menu is active and, if so, closes it
+function reasoningHomeClick() {
+    if ($('.js-slider-menu').hasClass('active')) {
+        toggleSliderMenu();
+    }
+}
+
+// this function listens for user to click on the home logo 
+// runs a function to determine if it should close the slider menu 
+function handleHomeClick() {
+    $('nav').on('click', '.name', function(event) {
+        console.log('`handleHomeClick` ran');
+        reasoningHomeClick();
+    });
+    console.log('`handleHomeClick` ran');
+}
+
+// this function listens for user to click on hamburger menu and calls the toggleSliderMenu function
+function handleSliderMenu() {
+    $('nav').on('click', '.fa-bars', function(event) {
+        console.log('`handleSliderMenu` ran');
+        toggleSliderMenu();
+    });
+    console.log('`handleSliderMenu` ran');
+}
+
+// this function listens for user to click on a menu item and calls the toggleSliderMenu function
+function handleSliderExit() {
+    $('.js-slider-menu').on('click', '.jumper', function(event) {
+        console.log('`handleSliderExit` ran');
+        toggleSliderMenu();
+    });
+    console.log('`handleSliderExit` ran');
+}
+
+function handleHamburgerMenu() {
+    handleSliderMenu();
+    handleSliderExit();
+    handleHomeClick();
+}
+
+$(handleHamburgerMenu);
